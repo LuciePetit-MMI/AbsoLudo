2cb010418f3100bfa134d67b3cdfd5106bacd40e
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2021-04-19 18:41:02 +0200

    Js tops jeux

--- dist/js/topj.js
+++ dist/js/topj.js
@@ -1,35 +1,39 @@
-var btns = document.querySelectorAll('h1 > div');
+var btns = document.querySelectorAll('#topj h1 > div');
 var userTop = document.querySelector('#user_top');
 var absoTop = document.querySelector('#absoludo_top');
 
 
-function clickSurBouton(){
+function clickedOnBtn(){
     const btn = this;
-    //ajoute .courant au slide suivant
+    //Show user top
         if (btn===user_title){
+            //change active class
             btn.classList.add('active');
             btn.nextElementSibling.classList.remove('active');
+            //show game user top
             userTop.classList.add('show');
             userTop.classList.remove('hidden');
+            //hide game absoludo top
             absoTop.classList.add('hidden');
             absoTop.classList.remove('show');
-
         }
 
-    //ajoute .courant au slide precedent
+    //Show absoludo top
         if (btn===absoludo_title){
+            //change active class
             btn.classList.add('active');
             btn.previousElementSibling.classList.remove('active');
+            //show game absoludo top
             absoTop.classList.add('show');
             absoTop.classList.remove('hidden');
+            //hide game user top
             userTop.classList.add('hidden');
             userTop.classList.remove('show');
-
         }
 }
 
 
 for (let i=0; i<btns.length; i++){
     const btn = btns[i];
-    btn.addEventListener('click', clickSurBouton);
+    btn.addEventListener('click', clickedOnBtn);
 }
