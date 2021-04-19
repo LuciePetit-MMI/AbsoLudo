cdfbb2888b5e0dc387abf6c4c4e7848a469fb2c7
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2021-04-19 18:41:02 +0200

    Revert "Js tops jeux"
    This reverts commit 2cb010418f3100bfa134d67b3cdfd5106bacd40e.

--- dist/js/topj.js
+++ dist/js/topj.js
@@ -1,39 +1,35 @@
-var btns = document.querySelectorAll('#topj h1 > div');
+var btns = document.querySelectorAll('h1 > div');
 var userTop = document.querySelector('#user_top');
 var absoTop = document.querySelector('#absoludo_top');
 
 
-function clickedOnBtn(){
+function clickSurBouton(){
     const btn = this;
-    //Show user top
+    //ajoute .courant au slide suivant
         if (btn===user_title){
-            //change active class
             btn.classList.add('active');
             btn.nextElementSibling.classList.remove('active');
-            //show game user top
             userTop.classList.add('show');
             userTop.classList.remove('hidden');
-            //hide game absoludo top
             absoTop.classList.add('hidden');
             absoTop.classList.remove('show');
+
         }
 
-    //Show absoludo top
+    //ajoute .courant au slide precedent
         if (btn===absoludo_title){
-            //change active class
             btn.classList.add('active');
             btn.previousElementSibling.classList.remove('active');
-            //show game absoludo top
             absoTop.classList.add('show');
             absoTop.classList.remove('hidden');
-            //hide game user top
             userTop.classList.add('hidden');
             userTop.classList.remove('show');
+
         }
 }
 
 
 for (let i=0; i<btns.length; i++){
     const btn = btns[i];
-    btn.addEventListener('click', clickedOnBtn);
+    btn.addEventListener('click', clickSurBouton);
 }
