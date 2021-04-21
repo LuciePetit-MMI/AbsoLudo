c3e7ff2bf505b992f98333113c7a67d7ab341ca4
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2021-04-21 11:41:08 +0200

    Dev Front thanks page

---
+++ thanks.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>    
+        <?php require "./html/head.html"; ?>
+        <title>Remerciement | AbsoLudo</title>
+    </head>
+<body id="thanks">
+    <header>
+        <?php require "./html/nav.html"; ?>
+    <h1>Merci infiniment pour votre soutien</h1>
+    </header>
+    <h2>Les Mécènes</h2>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <h2>Les Méga Financeurs</h2>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <h2>Les Subventionneurs</h2>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <h2>Les Financeurs</h2>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <h2>Les Contributeurs</h2>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    <p>Nom Prénom</p>
+    
+    <?php require "./html/footer.html"; ?>
+    <?php require "./html/scripts.html"; ?>
+</body>
+</html>
