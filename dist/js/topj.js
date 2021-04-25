0c8e999e71c8bc90fde3a12a182f91a214b815de
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2021-04-25 13:34:14 +0200

    Corrections

--- dist/js/topj.js
+++ dist/js/topj.js
@@ -1,4 +1,4 @@
-var btns = document.querySelectorAll('h1 > div');
+var btns = document.querySelectorAll('#topj h1 > div');
 var userTop = document.querySelector('#user_top');
 var absoTop = document.querySelector('#absoludo_top');
 
